8553023cef305c36834caa681edddeec4b6b22d7
Sumit Sahoo <sumitsahoo1988@gmail.com>
2023-03-24 14:51:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import FileEncrypter from './pages/FileEncrypter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <FileEncrypter/>
   )
